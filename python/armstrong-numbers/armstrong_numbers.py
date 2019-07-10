817abc4540c6e5c804c27b0d0fe4ed16799ce065
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-07-09 17:48:44 -0700

    [implemented and passed] panagram function

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,6 +1,6 @@
 def is_armstrong_number(number):
     """
-        is a number that is the sum of its own digits each 
+        armstrong number is a number that is the sum of its own digits each 
         raised to the power of the number of digits.
     """
     pass
--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,4 +1,23 @@
+import re, string
 def is_pangram(sentence):
     """Determine if a sentence is a pangram. A pangram (every letter) 
-    is a sentence using every letter of the alphabet at least once."""
-    pass
+    is a sentence using every letter of the alphabet at least once.
+    Runtime: O(n) where n number of chars
+    """
+    
+    # remove all chars/space except letters and lowercase them
+    norm_sent = ''.join(filter(str.isalpha, sentence)).lower() # O(n)
+    
+    # create a set from the sentence
+    norm_sent = set(norm_sent) # O(n)
+    
+    # check if all letters occured
+    return len(norm_sent) == 26 # O(1)
+
+
+
+
+
+
+sentence = "the quick brown fox jumps over the lazy dog"
+print(is_pangram(sentence))
